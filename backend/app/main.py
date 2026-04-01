8973e59d04b9473f8fc6804b3f9f5ec7ddcf1399
Jakub Rosa <jakub.rosa03@gmail.com>
2026-04-02 01:51:18 +0200

    small fixes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from ..app.api.routes import router
+from app.api.routes import router
 
 app = FastAPI(
     title='FITRPG Backend Api',
@@ -19,4 +19,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(router, prefix='api')
+app.include_router(router, prefix='/api')
