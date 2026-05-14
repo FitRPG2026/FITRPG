a03a52fbf8225a1793ef152497f6d5356a43007b
MD04-CFC <maciek.dabrowski.2004@gmail.com>
2026-05-14 13:49:24 +0200

    path clinet

--- frontend/WebApp/src/app/app.routes.server.ts
+++ frontend/WebApp/src/app/app.routes.server.ts
@@ -13,5 +13,5 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   { path: 'login',    renderMode: RenderMode.Prerender },
   { path: 'register', renderMode: RenderMode.Prerender },
-  { path: '**',       renderMode: RenderMode.Server },
+  { path: '**',       renderMode: RenderMode.Client },
 ];
