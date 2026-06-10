e8b9721cbe49ea20ba7a40440948612dbcb03897
Antoni Szymański <antoszympoczta@gmail.com>
2026-06-10 13:07:01 +0000

    chore: usun nieuzywany import ToastContainerComponent z AppComponent
    ToastContainer renderowany jest w dashboardzie, w AppComponent byl martwy
    import (warning NG8113).

--- frontend/WebApp/src/app/app.ts
+++ frontend/WebApp/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToastContainerComponent } from './components/toast-container/toast-container';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToastContainerComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
