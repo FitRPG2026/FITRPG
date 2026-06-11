6f1d2360fd692602c72d63ccd29113e197cd7b05
jakubdabrowskii <jakubdabrowski818@gmail.com>
2026-06-11 19:41:49 +0200

    add splash-screen

---
+++ frontend/WebApp/src/app/components/splash-screen/splash-screen.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, OnDestroy, inject, signal, output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { timer, switchMap, catchError, of, filter, take, Subscription } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-splash-screen',
+  standalone: true,
+  templateUrl: './splash-screen.component.html',
+  styleUrl: './splash-screen.component.css'
+})
+export class SplashScreen implements OnInit, OnDestroy {
+  private http = inject(HttpClient);
+  private subs = new Subscription();
+
+  serverReady = output<void>();
+  
+  currentText = signal('Nawiązywanie połączenia z serwerem bazowym...');
+
+  private loadingTexts = [
+    "Wczytywanie statystyk...",
+    "Parzenie kawy dla sztucznej inteligencji...",
+    "Trwa to dłużej niż zwykle, rozgrzewamy silniki chmurowe..."
+  ];
+
+  ngOnInit() {
+    this.subs.add(
+      timer(10000, 10000).subscribe((val) => {
+        const index = val % this.loadingTexts.length;
+        this.currentText.set(this.loadingTexts[index]);
+      })
+    );
+
+    this.subs.add(
+      timer(0, 5000).pipe(
+        switchMap(() => this.http.get(`${environment.apiUrl}/api/health`, { responseType: 'text' }).pipe(
+          catchError(() => of(null))
+        )),
+        filter(response => response !== null),
+        take(1)
+      ).subscribe(() => {
+        this.serverReady.emit();
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+}
