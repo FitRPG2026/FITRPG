c708b6c556ffe4e52faf65652a3b750b127ed714
jakubdabrowskii <jakubdabrowski818@gmail.com>
2026-06-12 07:49:34 +0200

    fixed splash-screen

--- frontend/WebApp/src/app/app.ts
+++ frontend/WebApp/src/app/app.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SplashScreen } from './components/splash-screen/splash-screen';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SplashScreen],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class AppComponent { }
+export class AppComponent {
+
+  isServerReady = signal(false);
+
+  onServerReady() {
+    this.isServerReady.set(true);
+  }
+
+ }
